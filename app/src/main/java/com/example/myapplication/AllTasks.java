ed5afe706e95aa1434b3d21355fba3a1b5f501bf
AnasAlRamahi <anas_alramahi@outlook.com>
2021-08-30 08:28:32 +0300

    added the android application into the repository

---
+++ app/src/main/java/com/example/myapplication/AllTasks.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AllTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks);
+
+        Button backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToMainActivity = new Intent(AllTasks.this, MainActivity.class);
+                startActivity(backToMainActivity);
+            }
+        });
+    }
+}
